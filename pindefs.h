45ad8a76bffdcfba327f03c5fe946f79187205c3
Joey Steele <joeysteele2001@gmail.com>
2021-02-21 12:53:44 -0500

    Make pin definition header file

---
+++ pindefs.h
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+
+namespace PinDefs {
+    constexpr int PIN_SLIDE_TOP = A3;
+    constexpr int PIN_SLIDE_BOTTOM = A6;
+    constexpr int PIN_FSR_NECK = A1;
+    constexpr int PIN_FSR_BODY = A0;
+    constexpr int PIN_SWITCH = A2;
+    constexpr int PIN_DIAL = A7;
+    constexpr int PIN_VCA = 3;
+    constexpr int PIN_VCF = 4;
+    constexpr int PIN_VCO = 5;
+    constexpr int PIN_LFO = 6;
+}
