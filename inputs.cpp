2d1a8d424885ad8c42460168103504a3d65f5c8f
Joey Steele <joeysteele2001@gmail.com>
2021-02-21 13:25:49 -0500

    Write skeletal inputs.cpp

---
+++ inputs.cpp
@@ -0,0 +1,20 @@
+#include "inputs.h"
+#include "pindefs.h"
+
+// TODO define readSlideTop()
+float readSlideTop();
+
+// TODO define readSlideBottom()
+float readSlideBottom();
+
+// TODO define readFsrNeck()
+float readFsrNeck();
+
+// TODO define readFsrBody()
+float readFsrBody();
+
+// TODO define readSwitch()
+bool readSwitch();
+
+// TODO define readDial()
+bool readDial();
