0fef5d8e17d81931248146ed24f3ca1c9f52fc57
Joey Steele <joeysteele2001@gmail.com>
2021-02-21 15:19:37 -0500

    Change dial pin to A8

--- pindefs.h
+++ pindefs.h
@@ -6,7 +6,7 @@ namespace PinDefs {
     constexpr uint8_t PIN_FSR_NECK = A1;
     constexpr uint8_t PIN_FSR_BODY = A0;
     constexpr uint8_t PIN_SWITCH = A2;
-    constexpr uint8_t PIN_DIAL = A7;
+    constexpr uint8_t PIN_DIAL = A8;
     constexpr uint8_t PIN_VCA = 3;
     constexpr uint8_t PIN_VCF = 4;
     constexpr uint8_t PIN_VCO = 5;
